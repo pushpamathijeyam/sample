ca63463b62195b18859d4321d2a0f56948a768bd
pushpamathijeyam <pushpamathi90@gmail.com>
2023-03-29 13:05:39 +0530

    done by pushpamathi

--- src/org/gitoperation/gitsample.java
+++ src/org/gitoperation/gitsample.java
@@ -1,8 +1,15 @@
 package org.gitoperation;
 
 public class gitsample {
-	private void print() {
-System.out.println("Enter your name:pushpamathi");
+	private void add(int a,int b) {
+		int c=a+b;
+		System.out.println("sum of two number:"+c);
+		
+	}
+	
+	public static void main(String[] args) {
+		gitsample gs=new gitsample();
+		gs.add(10, 20);
 	}
 
 }
