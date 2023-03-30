3b9ba922b1337def17fe6cb6daa7ade085eeb3da
pushpamathijeyam <pushpamathi90@gmail.com>
2023-03-30 12:32:22 +0530

    updated by amirtha

--- src/org/gitoperation/gitsample.java
+++ src/org/gitoperation/gitsample.java
@@ -3,6 +3,10 @@ package org.gitoperation;
 public class gitsample {
 	private void print() {
 System.out.println("Enter your name:pushpamathi");
-	}
 
+	}
+	private void sub(int a1,int b1) {
+int su=a1-b1;
+System.out.println(su);
+	}
 }
